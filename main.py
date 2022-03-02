3d2bd671953ecb64cb1f0302b259dc5b9b95dba3
DystDev <58528580+DystDev@users.noreply.github.com>
2022-03-02 21:51:01 +0000

    Fix decider order
    - Format document
    - Change order of getSignifiers to least specific to most specific

--- main.py
+++ main.py
@@ -14,238 +14,237 @@
 # DONE - Add questions to be posed to user
 
 # Imports
-import random # Add variation to the bot
-from enum import Enum # Custom query types
-import time # For waiting a bit -> more natural
+import random  # Add variation to the bot
+from enum import Enum  # Custom query types
+import time  # For waiting a bit -> more natural
 from api import fetchWikipedia
 from storedData import iden, opinions, phrases
 import helpers
 # Lists
-punctuation = ["?",".",","]
+punctuation = ["?", ".", ","]
 queryIdentity = ['you', 'your']
 queryOpinionVerb = ['do you']
-queryOpinionFeature = ['are you a','are you', 'are you the']
-queryWiki = ['who is', 'whos', "who's", 'what is a', 'what is an', 'search up', 'define', 'what is the meaning of', 'who are']
+queryOpinionFeature = ['are you a', 'are you', 'are you the']
+queryWiki = ['who is', 'whos', "who's", 'what is a', 'what is an',
+             'search up', 'define', 'what is the meaning of', 'who are']
 
 # Query types enum
+
+
 class qTypes(Enum):
-  IDENTITY = 'IDENTITY'
-  OPINIONVERB = 'OPINIONVERB'
-  OPINIONFEATURE = 'OPINIONFEATURE'
-  WIKI = 'WIKI'
+    IDENTITY = 'IDENTITY'
+    OPINIONVERB = 'OPINIONVERB'
+    OPINIONFEATURE = 'OPINIONFEATURE'
+    WIKI = 'WIKI'
+
 
 # API Calls
 dictEndpoint = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
 
-class Bot: # The main bot class
-  def __init__(self): # Constructor
-    pass
 
-  # Main method. Goes through steps of handling conversation.
-  # PARAMS: N/A
-  def handleConversation(self): 
-    self.askQuestion()
-    self.getQueryType()
-    self.composeResponse()
+class Bot:  # The main bot class
+    def __init__(self):  # Constructor
+        pass
 
-  # Poses question to user (complicated way of calling input()). If question
-  # is the first asked, resets the bots and sets the local fields
-  # PARAMS: Prompt: Used for the prompt used for the input.
-  def askQuestion(self, prompt="What is your question? "): 
-    # Resets fields for each new question
-    ans = input(prompt)
-    if self.isNewQuestion == True:
-      self.reset()
-      self.isNewQuestion == False
-    if self.usrMsg == '' and self.usrMsgFormat == '':
-      self.usrMsg = self.formatAnswer(ans)
-      self.usrMsgFormat = self.formatAnswerArray(ans)
-    return ans
+    # Main method. Goes through steps of handling conversation.
+    # PARAMS: N/A
+    def handleConversation(self):
+        self.askQuestion()
+        self.getQueryType()
+        self.composeResponse()
 
-  # PARAMS: Prompt: Used for the prompt used for the input.
-  def getQueryType(self):
-    self.findSignifierFromArray(queryIdentity, qTypes.IDENTITY)
-    self.findSignifierFromArray(queryWiki, qTypes.WIKI)
-    self.findSignifierFromArray(queryOpinionFeature, qTypes.OPINIONFEATURE)
-    self.findSignifierFromArray(queryOpinionVerb, qTypes.OPINIONVERB)
-    # If the bot cannot find something to talk about, sends a random 
-    # misunderstand message and TODO poses question to user
-    last = self.usrMsgFormat[len(self.usrMsgFormat) -1]
-    # If the topic has not been found, or the last word of the question
-    # is the keyword, then find more broad topic.
-    if self.queryType == None or last == self.lastKeyword: 
-      self.backupFindTopic()
-    if self.queryType == None:
-      helpers.error()
-      return
-    return 
+    # Poses question to user (complicated way of calling input()). If question
+    # is the first asked, resets the bots and sets the local fields
+    # PARAMS: Prompt: Used for the prompt used for the input.
+    def askQuestion(self, prompt="What is your question? "):
+        # Resets fields for each new question
+        ans = input(prompt)
+        if self.isNewQuestion == True:
+            self.reset()
+            self.isNewQuestion == False
+        if self.usrMsg == '' and self.usrMsgFormat == '':
+            self.usrMsg = self.formatAnswer(ans)
+            self.usrMsgFormat = self.formatAnswerArray(ans)
+        return ans
 
-  # Prompts for answer question to user
-  
-
-
-  # Will remove punctuation and lower case
-  def formatAnswer(self, ans):
-    if ans[len(ans)-1] == '?':
-      ans = ans[:len(ans)-1]
-    return ans.lower()
-  
-  # Same as last, but will turn into array
-  def formatAnswerArray(self, ans):
-    if ans[len(ans)-1] == '?':
-      ans = ans[:len(ans)-1]
-    return ans.lower().split(' ')
-
-
-  
-  def composeResponse(self):
-    if self.queryType == qTypes.WIKI:
-      # print('wik qeustion')
-      if self.query == '':
-        self.query = self.obtainQuery(-1, 1)
-      if self.query == None:
-        helpers.error()
-        return
-      queryGotten = fetchWikipedia.getFromWiki(self.query)
-      if queryGotten == None:
-        print(helpers.naturalSpeechComposer(phrases.unknownDictionaryTemplates, self.query))
-      else:
-        print(helpers.naturalSpeechComposer(phrases.wikiTemplates, queryGotten))
-    
-    if self.queryType == qTypes.IDENTITY:
-      # print('identiyy question')
-      if self.query == '': 
-        self.query = self.obtainQuery(1, 1)
-      if self.query == None or self.query not in iden.topics: # If the feature requested not in identity
-        helpers.error()
-        return
-      self.queryIdentity()
-    
-    if self.queryType == qTypes.OPINIONFEATURE:
-      if self.query == '':
-        self.query = self.obtainQuery(-1, 1)
-      if self.query == None:
-        helpers.error()
-        return
-      self.queryOpinionFeature()
-    
-    if self.queryType == qTypes.OPINIONVERB:
-      if self.query == '':
-        self.query = self.obtainQuery(-2, 1)
-      if self.query == None:
-        helpers.error()
-        return
-      self.queryOpinionVerb()
-      
-  def findSignifierFromArray(self, arrayOfSignifier, targetQueryType):
-    # Checks if query signifiers are in the user input
-    for phrase in arrayOfSignifier:
-      phraseSplit = phrase.split(' ')
-      if ' '.join(phraseSplit) in self.usrMsg:
-        # Saves query type (enum) and the used keyword for future use
-        self.queryType = targetQueryType
-        self.lastKeyword = phraseSplit[len(phraseSplit) - 1]
-  
-  def obtainQuery(self, dataNumberRequired, offset):
-    usedKeyword = self.lastKeyword
-    qArr = self.usrMsgFormat
-    if dataNumberRequired == 1:
-      try:
-        query = qArr[qArr.index(usedKeyword) + offset]
-      except:
-        return
-      return query
-    elif dataNumberRequired == -1:
-      try:
-        queryList = qArr[qArr.index(usedKeyword)+1:]
-        query = " ".join(queryList)
-      except:
-        return
-      return query
-    elif dataNumberRequired == -2: # For verb phrase
-      try:
-        queryList = []
-        queryList.append(qArr[qArr.index(usedKeyword)+1])
-        queryList.append(qArr[qArr.index(usedKeyword)+2:])
-      except:
-        return
-      return queryList
-
-  # More broad searching for topic, so may misunderstand and only works identity.
-  # However, it will work more often to find the topic.
-  def backupFindTopic(self):
-    for word in self.usrMsgFormat:
-      # iden.topics is found in iden.py and collects all the topics from the iden object.
-      if word in iden.topics:
-        self.queryType = qTypes.IDENTITY
-        self.query = word
-
-  def queryOpinionFeature(self):
-    # this code is pretty mank
-    if self.query not in opinions.opF.keys():
-      opinions.opF[self.query] = random.randint(-2, 2)
-    self.postResponse(phrases.featureOps[opinions.opF[self.query]], self.query)
-
-  
-  def queryOpinionVerb(self):
-    verb = self.query[0]
-    subject = ' '.join(self.query[1])
-    if verb not in opinions.opV.keys():
-      opinions.opV[verb] = {}
-    verbSubjects = opinions.opV[verb]
-    if subject not in verbSubjects.keys():
-      opinions.opV[verb][subject] = random.randint(-2, 2)
-    self.postResponse(phrases.verbOps[opinions.opV[verb][subject]], verb + ' ' + subject)
-
-  
-  def queryIdentity(self):
-    for tuple in iden.iden:
-      for value in tuple:
-        if self.query == value:
-          i = iden.iden[tuple]
-          responseTemplates = i['responses']
-          # Pretty disgusting way to see which are provided
-          try:
-            value = i['value']
-          except:
-            self.postResponse(responseTemplates)
+    # PARAMS: Prompt: Used for the prompt used for the input.
+    def getQueryType(self):
+        self.findSignifierFromArray(queryWiki, qTypes.WIKI)
+        self.findSignifierFromArray(queryOpinionVerb, qTypes.OPINIONVERB)
+        self.findSignifierFromArray(queryOpinionFeature, qTypes.OPINIONFEATURE)
+        self.findSignifierFromArray(queryIdentity, qTypes.IDENTITY)
+        # If the bot cannot find something to talk about, sends a random
+        # misunderstand message and TODO poses question to user
+        last = self.usrMsgFormat[len(self.usrMsgFormat) - 1]
+        # If the topic has not been found, or the last word of the question
+        # is the keyword, then find more broad topic.
+        if self.queryType == None or last == self.lastKeyword:
+            self.backupFindTopic()
+        if self.queryType == None:
+            helpers.error()
             return
-          try:
-            cb = i['callback']
-            cbRec = i['cbRecieved']
-          except:
-            self.postResponse(responseTemplates, value)
-            return
-          self.postResponse(responseTemplates, value, cb, cbRec)
-              
-  def postResponse(self, templates, val = None, cb = None, cbRec = None):
-    if val == None:
-      print(random.choice(templates))
-      time.sleep(0.5)
-      return
-    print(helpers.naturalSpeechComposer(templates, val))
-    time.sleep(0.5)
-    
-    if cb != None and cbRec != None:
-      usrResponse = self.askQuestion(random.choice(cb))
-      print(helpers.naturalSpeechComposer(cbRec, usrResponse))
-    
-  def reset(self):
-    # All examples after the question 'What is an apple?'
-    self.lastKeyword = '' # e.g. 'what is an'
-    self.usrMsg = '' # e.g. 'what is an apple'
-    self.usrMsgFormat = '' # e.g. ['what', 'is', 'an', 'apple']
-    self.queryType = None # e.g qTypes.WEB
-    self.query = '' # e.g. apple
-    self.isNewQuestion = True # e.g. wtf
-    
-  
-  
+        return
 
+    # Prompts for answer question to user
+
+    # Will remove punctuation and lower case
+
+    def formatAnswer(self, ans):
+        if ans[len(ans)-1] == '?':
+            ans = ans[:len(ans)-1]
+        return ans.lower()
+
+    # Same as last, but will turn into array
+    def formatAnswerArray(self, ans):
+        if ans[len(ans)-1] == '?':
+            ans = ans[:len(ans)-1]
+        return ans.lower().split(' ')
+
+    def composeResponse(self):
+        if self.queryType == qTypes.WIKI:
+            if self.query == '':
+                self.query = self.obtainQuery(-1, 1)
+            if self.query == None:
+                helpers.error()
+                return
+            queryGotten = fetchWikipedia.getFromWiki(self.query)
+            if queryGotten == None:
+                print(helpers.naturalSpeechComposer(
+                    phrases.unknownDictionaryTemplates, self.query))
+            else:
+                print(helpers.naturalSpeechComposer(
+                    phrases.wikiTemplates, queryGotten))
+
+        if self.queryType == qTypes.IDENTITY:
+            # print('identiyy question')
+            if self.query == '':
+                self.query = self.obtainQuery(1, 1)
+            if self.query == None or self.query not in iden.topics:  # If the feature requested not in identity
+                helpers.error()
+                return
+            self.queryIdentity()
+
+        if self.queryType == qTypes.OPINIONFEATURE:
+            if self.query == '':
+                self.query = self.obtainQuery(-1, 1)
+            if self.query == None:
+                helpers.error()
+                return
+            self.queryOpinionFeature()
+
+        if self.queryType == qTypes.OPINIONVERB:
+            if self.query == '':
+                self.query = self.obtainQuery(-2, 1)
+            if self.query == None:
+                helpers.error()
+                return
+            self.queryOpinionVerb()
+
+    def findSignifierFromArray(self, arrayOfSignifier, targetQueryType):
+        # Checks if query signifiers are in the user input
+        for phrase in arrayOfSignifier:
+            phraseSplit = phrase.split(' ')
+            if ' '.join(phraseSplit) in self.usrMsg:
+                # Saves query type (enum) and the used keyword for future use
+                self.queryType = targetQueryType
+                self.lastKeyword = phraseSplit[len(phraseSplit) - 1]
+
+    def obtainQuery(self, dataNumberRequired, offset):
+        usedKeyword = self.lastKeyword
+        qArr = self.usrMsgFormat
+        if dataNumberRequired == 1:
+            try:
+                query = qArr[qArr.index(usedKeyword) + offset]
+            except:
+                return
+            return query
+        elif dataNumberRequired == -1:
+            try:
+                queryList = qArr[qArr.index(usedKeyword)+1:]
+                query = " ".join(queryList)
+            except:
+                return
+            return query
+        elif dataNumberRequired == -2:  # For verb phrase
+            try:
+                queryList = []
+                queryList.append(qArr[qArr.index(usedKeyword)+1])
+                queryList.append(qArr[qArr.index(usedKeyword)+2:])
+            except:
+                return
+            return queryList
+
+    # More broad searching for topic, so may misunderstand and only works identity.
+    # However, it will work more often to find the topic.
+    def backupFindTopic(self):
+        for word in self.usrMsgFormat:
+            # iden.topics is found in iden.py and collects all the topics from the iden object.
+            if word in iden.topics:
+                self.queryType = qTypes.IDENTITY
+                self.query = word
+
+    def queryOpinionFeature(self):
+        # this code is pretty mank
+        if self.query not in opinions.opF.keys():
+            opinions.opF[self.query] = random.randint(-2, 2)
+        self.postResponse(
+            phrases.featureOps[opinions.opF[self.query]], self.query)
+
+    def queryOpinionVerb(self):
+        verb = self.query[0]
+        subject = ' '.join(self.query[1])
+        if verb not in opinions.opV.keys():
+            opinions.opV[verb] = {}
+        verbSubjects = opinions.opV[verb]
+        if subject not in verbSubjects.keys():
+            opinions.opV[verb][subject] = random.randint(-2, 2)
+        self.postResponse(
+            phrases.verbOps[opinions.opV[verb][subject]], verb + ' ' + subject)
+
+    def queryIdentity(self):
+        for tuple in iden.iden:
+            for value in tuple:
+                if self.query == value:
+                    i = iden.iden[tuple]
+                    responseTemplates = i['responses']
+                    # Pretty disgusting way to see which are provided
+                    try:
+                        value = i['value']
+                    except:
+                        self.postResponse(responseTemplates)
+                        return
+                    try:
+                        cb = i['callback']
+                        cbRec = i['cbRecieved']
+                    except:
+                        self.postResponse(responseTemplates, value)
+                        return
+                    self.postResponse(responseTemplates, value, cb, cbRec)
+
+    def postResponse(self, templates, val=None, cb=None, cbRec=None):
+        if val == None:
+            print(random.choice(templates))
+            time.sleep(0.5)
+            return
+        print(helpers.naturalSpeechComposer(templates, val))
+        time.sleep(0.5)
+
+        if cb != None and cbRec != None:
+            usrResponse = self.askQuestion(random.choice(cb))
+            print(helpers.naturalSpeechComposer(cbRec, usrResponse))
+
+    def reset(self):
+        # All examples after the question 'What is an apple?'
+        self.lastKeyword = ''  # e.g. 'what is an'
+        self.usrMsg = ''  # e.g. 'what is an apple'
+        self.usrMsgFormat = ''  # e.g. ['what', 'is', 'an', 'apple']
+        self.queryType = None  # e.g qTypes.WEB
+        self.query = ''  # e.g. apple
+        self.isNewQuestion = True  # e.g. wtf
 
 
 # Main program
-dan = Bot() # Instance of the bot
-dan.reset() # reset i.e. init most fields
+dan = Bot()  # Instance of the bot
+dan.reset()  # reset i.e. init most fields
 while True:
-  dan.handleConversation()
+    dan.handleConversation()
